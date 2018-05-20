1027cadbcb6f2bc66f218ae3fc1f7f664011e673
Nishant-Taksande <nick20j@gmail.com>
2018-05-20 09:49:44 -0400

    Adding project

---
+++ GitIntegration/src/test/java/com/org/selenium/GitIntegration/GitTest.java
@@ -0,0 +1,38 @@
+package com.org.selenium.GitIntegration;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class GitTest {
+
+	WebDriver driver;
+
+	@BeforeClass
+	public void beforeClass() {
+		System.out.println("Im in Before Class");
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void f() {
+		System.out.println("Im in Test Method");
+		driver.get("http://www.github.com");
+		GitHomePage ghp = new GitHomePage(driver);
+		ghp.verifyUI();
+	}
+
+	
+	@AfterClass
+	public void afterClass() {
+		System.out.println("Im in After Class");
+		driver.close();
+		driver.quit();
+	}
+
+}
